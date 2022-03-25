c020f93b7973b058a93038cf475840a3c31f753a
Septillihedron <resha3adnandaru@gmail.com>
2022-03-25 15:02:58 +0700

    Create NoneOfTypeException

---
+++ src/main/java/me/sepdron/data/yamlconfig/NoneOfTypeException.java
@@ -0,0 +1,25 @@
+package me.sepdron.data.yamlconfig;
+
+public class NoneOfTypeException extends RuntimeException {
+
+	private NoneOfTypeException(String message) {
+		super(message);
+	}
+
+	protected static NoneOfTypeException createNotExistException(String path) {
+		String message = String.format("%s does not exist", path);
+		return new NoneOfTypeException(message);
+	}
+	protected static NoneOfTypeException createTypeException(String path, String type) {
+		String message = String.format("%s does not exist or has type other than %s", path, type);
+		return new NoneOfTypeException(message);
+	}
+	protected static NoneOfTypeException createMapException(String path) {
+		String message = String.format("%s does not exist or is not a section", path);
+		return new NoneOfTypeException(message);
+	}
+	protected static NoneOfTypeException createListException(String path) {
+		String message = String.format("%s does not exist or is not a list", path);
+		return new NoneOfTypeException(message);
+	}
+}
