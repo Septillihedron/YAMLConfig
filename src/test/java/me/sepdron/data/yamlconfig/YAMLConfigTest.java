6d2af8817dff7884d9233ac6e724b72a676288fb
Septillihedron <resha3adnandaru@gmail.com>
2022-03-29 21:15:13 +0700

    Add tests for getInt

--- src/test/java/me/sepdron/data/yamlconfig/YAMLConfigTest.java
+++ src/test/java/me/sepdron/data/yamlconfig/YAMLConfigTest.java
@@ -113,6 +113,68 @@ public class YAMLConfigTest {
 		assertThrows(NoneOfTypeException.class, () -> listConfig.getString(2, true));
 	}
 
+	@Test
+	void mapMapping_getInteger_String_Test() {
+		assertEquals(topConfig.getInt("int"), 121);
+		assertEquals(topConfig.getInt("hyd"), 0);
+		assertEquals(topConfig.getInt("string"), 0);
+		assertEquals(topConfig.getInt("float"), 0);
+		assertEquals(topConfig.getInt("long"), 0);
+	}
+	@Test
+	void mapMapping_getInteger_String_String_Test() {
+		assertEquals(topConfig.getInt("int", -12), 121);
+		assertEquals(topConfig.getInt("hyd", -12), -12);
+		assertEquals(topConfig.getInt("string", -12), -12);
+		assertEquals(topConfig.getInt("float", -12), -12);
+		assertEquals(topConfig.getInt("long", -12), -12);
+	}
+	@Test
+	void mapMapping_getInteger_String_boolean_Test() {
+		assertEquals(topConfig.getInt("int", false), 121);
+		assertDoesNotThrow(() -> topConfig.getInt("hyd", false));
+		assertDoesNotThrow(() -> topConfig.getInt("string", false));
+		assertDoesNotThrow(() -> topConfig.getInt("float", false));
+		assertDoesNotThrow(() -> topConfig.getInt("long", false));
+
+		assertEquals(topConfig.getInt("int", true), 121);
+		assertThrows(NoneOfTypeException.class, () -> topConfig.getInt("hyd", true));
+		assertThrows(NoneOfTypeException.class, () -> topConfig.getInt("string", true));
+		assertThrows(NoneOfTypeException.class, () -> topConfig.getInt("float", true));
+		assertThrows(NoneOfTypeException.class, () -> topConfig.getInt("long", true));
+	}
+
+	@Test
+	void listMapping_getInteger_String_Test() {
+		assertEquals(listConfig.getInt(2), 1283); // 2 -> int value
+		assertEquals(listConfig.getInt(72), 0);
+		assertEquals(listConfig.getInt(3), 0);    // 3 -> string value
+		assertEquals(listConfig.getInt(9), 0);    // 9 -> double value
+		assertEquals(listConfig.getInt(10), 0);   // 10 -> long value
+	}
+	@Test
+	void listMapping_getInteger_String_String_Test() {
+		assertEquals(listConfig.getInt(2, -12), 1283); // 2 -> int value
+		assertEquals(listConfig.getInt(72, -12), -12);
+		assertEquals(listConfig.getInt(3, -12), -12);  // 3 -> string value
+		assertEquals(listConfig.getInt(9, -12), -12);  // 9 -> double value
+		assertEquals(listConfig.getInt(10, -12), -12); // 10 -> long value
+	}
+	@Test
+	void listMapping_getInteger_String_boolean_Test() {
+		assertEquals(listConfig.getInt(2, false), 1283); // 2 -> int value
+		assertDoesNotThrow(() -> listConfig.getInt(72, false));
+		assertDoesNotThrow(() -> listConfig.getInt(3, false));  // 3 -> string value
+		assertDoesNotThrow(() -> listConfig.getInt(9, false));  // 9 -> double value
+		assertDoesNotThrow(() -> listConfig.getInt(10, false)); // 10 -> long value
+
+		assertEquals(listConfig.getInt(2, true), 1283); // 2 -> int value
+		assertThrows(NoneOfTypeException.class, () -> listConfig.getInt(72, true));
+		assertThrows(NoneOfTypeException.class, () -> listConfig.getInt(3, true));  // 3 -> string value
+		assertThrows(NoneOfTypeException.class, () -> listConfig.getInt(9, true));  // 9 -> double value
+		assertThrows(NoneOfTypeException.class, () -> listConfig.getInt(10, true)); // 10 -> long value
+	}
+
 	public static void assertThrowsException(Throwable exception, Executable executable) {
 		try {
 			executable.execute();
